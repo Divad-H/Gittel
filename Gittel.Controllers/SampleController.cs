de167cfa6950094e45b4013162c005ec7cad0ad0
David Hübscher <huebschersdavid@gmail.com>
2023-09-01 00:10:15 +0200

    Generate dtos using TypeGen.

--- Gittel.Controllers/SampleController.cs
+++ Gittel.Controllers/SampleController.cs
@@ -1,8 +1,24 @@
 ﻿using ApiGenerator;
+using TypeGen.Core.TypeAnnotations;
 
 namespace Gittel.Controllers;
 
+[ExportTsClass]
+public record SampleResultDto
+{
+  public required string Text { get; init; }
+}
+
+[ExportTsClass]
+public record SampleRequestDto
+{
+  public required string Text { get; init; }
+}
+
 public class SampleController : IController
 {
-
+  public Task<SampleResultDto> SampleFunction(SampleRequestDto data, CancellationToken ct)
+  {
+    return Task.FromResult<SampleResultDto>(new() { Text = data.Text });
+  }
 }
