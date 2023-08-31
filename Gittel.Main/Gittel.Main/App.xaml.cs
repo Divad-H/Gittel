061d763aa2ff6be0bf372deae9184e3627bf90a0
David Hübscher <huebschersdavid@gmail.com>
2023-08-31 23:52:02 +0200

    Change indentation to 2 spaces in C#.

--- Gittel.Main/Gittel.Main/App.xaml.cs
+++ Gittel.Main/Gittel.Main/App.xaml.cs
@@ -6,23 +6,23 @@ namespace Gittel.Main;
 
 public partial class App : Application
 {
-    public App()
-    {
-        this.InitializeComponent();
-    }
+  public App()
+  {
+    this.InitializeComponent();
+  }
 
-    /// <summary>
-    /// Invoked when the application is launched normally by the end user.  Other entry points
-    /// will be used such as when the application is launched to open a specific file.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        var commandLineArgs = Parser.Default.ParseArguments<CommandLineOptions>(Environment.GetCommandLineArgs());
-        
-        m_window = new MainWindow(commandLineArgs.Value.SpaUri);
-        m_window.Activate();
-    }
+  /// <summary>
+  /// Invoked when the application is launched normally by the end user.  Other entry points
+  /// will be used such as when the application is launched to open a specific file.
+  /// </summary>
+  /// <param name="args">Details about the launch request and process.</param>
+  protected override void OnLaunched(LaunchActivatedEventArgs args)
+  {
+    var commandLineArgs = Parser.Default.ParseArguments<CommandLineOptions>(Environment.GetCommandLineArgs());
 
-    private Window m_window;
+    m_window = new MainWindow(commandLineArgs.Value.SpaUri);
+    m_window.Activate();
+  }
+
+  private Window m_window;
 }
--- TsClientGenerator/IMessaging.cs
+++ TsClientGenerator/IMessaging.cs
@@ -5,14 +5,14 @@
 /// </summary>
 public interface IMessaging
 {
-    /// <summary>
-    /// An observable that emits a json value when a message is received
-    /// </summary>
-    IObservable<string> MessageReceivedObservable { get; }
+  /// <summary>
+  /// An observable that emits a json value when a message is received
+  /// </summary>
+  IObservable<string> MessageReceivedObservable { get; }
 
-    /// <summary>
-    /// Post a message (json) that will be sent
-    /// </summary>
-    /// <param name="message">The (json) message to be sent</param>
-    void PostMessage(string message);
+  /// <summary>
+  /// Post a message (json) that will be sent
+  /// </summary>
+  /// <param name="message">The (json) message to be sent</param>
+  void PostMessage(string message);
 }
