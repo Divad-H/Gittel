16b2edc55fe6514f913a099cbca763eb10e00ae5
David Hübscher <huebschersdavid@gmail.com>
2023-09-21 00:44:55 +0200

    Create a record GitOid.

---
+++ Libgit2Bindings/GitOid.cs
@@ -0,0 +1,8 @@
+﻿namespace Libgit2Bindings;
+
+public record GitOid(byte[] Id)
+{
+  public const int Size = 20;
+
+  public byte[] Id { get; } = Id.Length == Size ? Id : throw new ArgumentException($"Length of {nameof(Id)} must be {Size}.");
+}
