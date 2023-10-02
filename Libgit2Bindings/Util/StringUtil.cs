bbcd97c9ca6f8175a13466620583862e66a07c49
David Hübscher <huebschersdavid@gmail.com>
2023-10-03 01:40:14 +0200

    Add a conversion function from GitBuf to byte[].

--- Libgit2Bindings/Util/StringUtil.cs
+++ Libgit2Bindings/Util/StringUtil.cs
@@ -20,4 +20,11 @@ internal static class StringUtil
     var span = MemoryMarshal.CreateReadOnlySpanFromNullTerminated((byte*)utf8String);
     return Encoding.UTF8.GetString(span);
   }
+
+  public static unsafe byte[] ToArray(libgit2.GitBuf buf)
+  {
+    var bytes = new byte[buf.Size];
+    Marshal.Copy((IntPtr)buf.Ptr, bytes, 0, (int)buf.Size);
+    return bytes;
+  }
 }
