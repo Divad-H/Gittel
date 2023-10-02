c9575f018f34d598ef585c2fe087f8f36fb47108
David Hübscher <huebschersdavid@gmail.com>
2023-10-02 23:24:34 +0200

    Add an enum BranchType.

--- Libgit2Bindings/IGitReference.cs
+++ Libgit2Bindings/IGitReference.cs
@@ -1,5 +1,13 @@
 ﻿namespace Libgit2Bindings;
 
+[Flags]
+public enum BranchType
+{
+  LocalBranch = 1 << 0,
+  RemoteBranch = 1 << 1,
+  All = 3,
+}
+
 public interface IGitReference : IDisposable
 {
 }
---
+++ Libgit2Bindings/Mappers/BranchTypeMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class BranchTypeMapper
+{
+  public static libgit2.GitBranchT ToNative(BranchType branchType)
+  {
+    libgit2.GitBranchT nativeFlags = 0;
+    if (branchType == BranchType.LocalBranch)
+      nativeFlags |= libgit2.GitBranchT.GIT_BRANCH_LOCAL;
+    if (branchType == BranchType.RemoteBranch)
+      nativeFlags |= libgit2.GitBranchT.GIT_BRANCH_REMOTE;
+    if (branchType == BranchType.All)
+      nativeFlags |= libgit2.GitBranchT.GIT_BRANCH_ALL;
+    return nativeFlags;
+  }
+}
