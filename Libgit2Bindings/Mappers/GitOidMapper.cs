df4bf2116f2c57d7704f2d098c776e7e084dec2f
David Hübscher <huebschersdavid@gmail.com>
2023-10-06 00:04:48 +0200

    GitOidMapper: add missing create from short id function.

--- Libgit2Bindings/Mappers/GitOidMapper.cs
+++ Libgit2Bindings/Mappers/GitOidMapper.cs
@@ -15,4 +15,15 @@ internal static class GitOidMapper
     Marshal.Copy(oid.Id, 0, (IntPtr)data.id, oid.Id.Length);
     return libgit2.GitOid.__CreateInstance(data);
   }
+
+  public static GitOid FromShortId(byte[] shortId)
+  {
+    if (shortId.Length > 20)
+    {
+      throw new ArgumentException("Length of shortId must be less than or equal to 20.");
+    }
+    byte[] id = new byte[20];
+    Array.Copy(shortId, id, shortId.Length);
+    return new GitOid(id);
+  }
 }
