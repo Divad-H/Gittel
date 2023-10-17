07cfbe3cbb616636f2c9cad74e4aed495c786cb9
David Hübscher <huebschersdavid@gmail.com>
2023-10-18 00:02:13 +0200

    Create GitRemote.

---
+++ Libgit2Bindings/GitRemote.cs
@@ -0,0 +1,33 @@
+﻿namespace Libgit2Bindings;
+
+internal sealed class GitRemote : IGitRemote
+{
+  public libgit2.GitRemote NativeGitRemote { get; }
+  public GitRemote(libgit2.GitRemote nativeGitRemote)
+  {
+    NativeGitRemote = nativeGitRemote;
+  }
+
+  #region IDisposable Support
+  private bool _disposedValue;
+  private void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      libgit2.remote.GitRemoteFree(NativeGitRemote);
+      _disposedValue = true;
+    }
+  }
+
+  ~GitRemote()
+  {
+    Dispose(disposing: false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+  #endregion
+}
