61da5b1beef41775be085f3c6aaca2876a0766e1
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:30:39 +0200

    Add GitRemoteDirectionMapper.

---
+++ Libgit2Bindings/Mappers/GitRemoteDirectionMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class GitRemoteDirectionMapper
+{
+  public static GitRemoteDirection FromNative(libgit2.GitDirection nativeGitRemoteDirection)
+  {
+    return nativeGitRemoteDirection switch
+    {
+      libgit2.GitDirection.GIT_DIRECTION_FETCH => GitRemoteDirection.Fetch,
+      libgit2.GitDirection.GIT_DIRECTION_PUSH => GitRemoteDirection.Push,
+      _ => throw new ArgumentOutOfRangeException(nameof(nativeGitRemoteDirection), nativeGitRemoteDirection, null)
+    };
+  }
+}
