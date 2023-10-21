a142c19707c865c5b83a97ec825380a8318c30fd
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:20:06 +0200

    Create GitTransport.

---
+++ Libgit2Bindings/GitTransport.cs
@@ -0,0 +1,43 @@
+﻿namespace Libgit2Bindings;
+
+internal class GitTransport : IGitTransport
+{
+  private bool _ownsNativeInstance = true;
+  public libgit2.GitTransport NativeGitTransport { get; }
+
+  public GitTransport(libgit2.GitTransport nativeGitTransport)
+  {
+    NativeGitTransport = nativeGitTransport;
+  }
+
+  public void Release()
+  {
+    _ownsNativeInstance = false;
+  }
+
+  #region IDisposable Support
+  private bool _disposedValue;
+  private void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (_ownsNativeInstance)
+      {
+        NativeGitTransport.Free?.Invoke(NativeGitTransport.__Instance);
+      }
+      NativeGitTransport.Dispose();
+    }
+  }
+
+  ~GitTransport()
+  {
+    Dispose(disposing: false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+  #endregion
+}
