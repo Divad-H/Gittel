9163c0493f138774af3efe4b9eb8cedb56c95a02
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:31:20 +0200

    Add GitPackbuilderStageMapper.

---
+++ Libgit2Bindings/Mappers/PackbuilderStageMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class PackbuilderStageMapper
+{
+  public static PackbuilderStage ToManaged(this libgit2.GitPackbuilderStageT nativeStage)
+  {
+    return nativeStage switch 
+    { 
+      libgit2.GitPackbuilderStageT.GIT_PACKBUILDER_ADDING_OBJECTS => PackbuilderStage.AddingObjects,
+      libgit2.GitPackbuilderStageT.GIT_PACKBUILDER_DELTAFICATION => PackbuilderStage.Deltafication, 
+      _ => throw new ArgumentOutOfRangeException(nameof(nativeStage), nativeStage, null) };
+  }
+}
