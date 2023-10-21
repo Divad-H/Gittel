b94d0facdbd9aeb7beed2ff7c6b0a681c80c6612
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:31:47 +0200

    Add a utility class DisposableCollection.

---
+++ Libgit2Bindings/Util/DisposableCollection.cs
@@ -0,0 +1,28 @@
+﻿namespace Libgit2Bindings.Util;
+
+internal class DisposableCollection : IDisposable
+{
+  private readonly List<IDisposable> _disposables = new();
+
+  public void Add(IDisposable disposable)
+  {
+    _disposables.Add(disposable);
+  }
+
+  public void Dispose()
+  {
+    foreach (var disposable in _disposables.Reverse<IDisposable>())
+    {
+      disposable.Dispose();
+    }
+  }
+}
+
+internal static class DisposableCollectionExtensions
+{
+  public static T DisposeWith<T>(this T disposable, DisposableCollection disposables) where T : IDisposable
+  {
+    disposables.Add(disposable);
+    return disposable;
+  }
+}
