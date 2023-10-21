2249cb4cc288ffe3c5e8207c8e837052bc614cf4
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:30:02 +0200

    Add GitIndexerProgressMapper

---
+++ Libgit2Bindings/Mappers/GitIndexerProgressMapper.cs
@@ -0,0 +1,24 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class GitIndexerProgressMapper
+{
+  public static GitIndexerProgress? FromNativePtr(IntPtr nativeGitIndexerProgressPtr)
+  {
+    if (nativeGitIndexerProgressPtr == IntPtr.Zero)
+      return null;
+    using var nativeGitIndexerProgress = libgit2.GitIndexerProgress.__CreateInstance(nativeGitIndexerProgressPtr);
+    return FromNative(nativeGitIndexerProgress);
+  }
+
+  public static GitIndexerProgress FromNative(libgit2.GitIndexerProgress nativeGitIndexerProgress)
+  {
+    return new(
+      TotalObjects: nativeGitIndexerProgress.TotalObjects,
+      IndexedObjects: nativeGitIndexerProgress.IndexedObjects,
+      ReceivedObjects: nativeGitIndexerProgress.ReceivedObjects,
+      LocalObjects: nativeGitIndexerProgress.LocalObjects,
+      TotalDeltas: nativeGitIndexerProgress.TotalDeltas,
+      IndexedDeltas: nativeGitIndexerProgress.IndexedDeltas,
+      ReceivedBytes: nativeGitIndexerProgress.ReceivedBytes);
+  }
+}
