45ede712c7238b83cd83d2cb2570908e94852f69
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:19:32 +0200

    Create GitCertificate.

---
+++ Libgit2Bindings/GitCertificate.cs
@@ -0,0 +1,33 @@
+﻿namespace Libgit2Bindings;
+
+internal class GitCertificate : IGitCertificate
+{
+  public libgit2.GitCert NativeGitCertificate { get; }
+
+  public GitCertificate(libgit2.GitCert nativeGitCertificate)
+  {
+    NativeGitCertificate = nativeGitCertificate;
+  }
+
+  #region IDisposable Support
+  private bool _disposedValue;
+  private void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      NativeGitCertificate.Dispose();
+    }
+  }
+
+  ~GitCertificate()
+  {
+    Dispose(disposing: false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+  #endregion
+}
