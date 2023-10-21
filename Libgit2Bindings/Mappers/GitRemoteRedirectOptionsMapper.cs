c30170954fc8af34b64eefe9e9b8bb0a071dadf8
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:30:52 +0200

    Add GitRemoteRedirectOptionsMapper.

---
+++ Libgit2Bindings/Mappers/GitRemoteRedirectOptionsMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class GitRemoteRedirectOptionsMapper
+{
+  public static libgit2.GitRemoteRedirectT ToNative(this GitRemoteRedirectOptions managedOptions)
+  {
+    return managedOptions switch
+    {
+      GitRemoteRedirectOptions.Unspecified => 0,
+      GitRemoteRedirectOptions.RedirectNone => libgit2.GitRemoteRedirectT.GIT_REMOTE_REDIRECT_NONE,
+      GitRemoteRedirectOptions.RedirectInitial => libgit2.GitRemoteRedirectT.GIT_REMOTE_REDIRECT_INITIAL,
+      GitRemoteRedirectOptions.RedirectAll => libgit2.GitRemoteRedirectT.GIT_REMOTE_REDIRECT_ALL,
+      _ => throw new ArgumentOutOfRangeException(nameof(managedOptions), managedOptions, null)
+    };
+  }
+}
