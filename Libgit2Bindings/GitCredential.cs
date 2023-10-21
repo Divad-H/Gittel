31ebe2e16fbe204af7b66d0840c6d303a64374a3
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:19:56 +0200

    Create GitCredential.

---
+++ Libgit2Bindings/GitCredential.cs
@@ -0,0 +1,43 @@
+﻿namespace Libgit2Bindings;
+
+internal sealed class GitCredential : IGitCredential
+{
+  private bool _ownsNativeInstance = true;
+  public libgit2.GitCredential NativeGitCredential { get; }
+
+  public GitCredential(libgit2.GitCredential nativeGitCredential)
+  {
+    NativeGitCredential = nativeGitCredential;
+  }
+
+  public void Release()
+  {
+    _ownsNativeInstance = false;
+  }
+
+  #region IDisposable Support
+  private bool _disposedValue;
+  private void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (_ownsNativeInstance)
+      {
+        libgit2.credential.GitCredentialFree(NativeGitCredential);
+      }
+      NativeGitCredential.Dispose();
+    }
+  }
+
+  ~GitCredential()
+  {
+    Dispose(disposing: false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+  #endregion
+}
---
+++ Libgit2Bindings/IGitCredential.cs
@@ -0,0 +1,17 @@
+﻿namespace Libgit2Bindings;
+
+[Flags]
+public enum GitCredentialType
+{
+  UserPassPlaintext = 1 << 0,
+  SshKey = 1 << 1,
+  SshCustom = 1 << 2,
+  Default = 1 << 3,
+  SshInteractive = 1 << 4,
+  Username = 1 << 5,
+  SshMemory = 1 << 6,
+}
+
+public interface IGitCredential : IDisposable
+{
+}
