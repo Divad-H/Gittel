757bdd0cc4db361abac6dbe958278cb2d268e0b0
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:30:28 +0200

    Add GitRemoteCompletionMapper.

---
+++ Libgit2Bindings/Mappers/GitRemoteCompletionMapper.cs
@@ -0,0 +1,26 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class GitRemoteCompletionMapper
+{
+  public static libgit2.GitRemoteCompletionT ToNative(this RemoteCompletionType managedCompletion)
+  {
+    return managedCompletion switch
+    {
+      RemoteCompletionType.Download => libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_DOWNLOAD,
+      RemoteCompletionType.Indexing => libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_INDEXING,
+      RemoteCompletionType.Error => libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_ERROR,
+      _ => throw new ArgumentOutOfRangeException(nameof(managedCompletion), managedCompletion, null)
+    };
+  }
+
+  public static RemoteCompletionType ToManaged(this libgit2.GitRemoteCompletionT nativeCompletion)
+  {
+    return nativeCompletion switch
+    {
+      libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_DOWNLOAD => RemoteCompletionType.Download,
+      libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_INDEXING => RemoteCompletionType.Indexing,
+      libgit2.GitRemoteCompletionT.GIT_REMOTE_COMPLETION_ERROR => RemoteCompletionType.Error,
+      _ => throw new ArgumentOutOfRangeException(nameof(nativeCompletion), nativeCompletion, null)
+    };
+  }
+}
