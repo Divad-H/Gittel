d9b9e5e464ed9e928a71c8c8af94bb309389e8a6
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:29:33 +0200

    Add GitPushUpdateMapper

---
+++ Libgit2Bindings/Mappers/GitPushUpdateMapper.cs
@@ -0,0 +1,25 @@
+﻿using Libgit2Bindings.Util;
+
+namespace Libgit2Bindings.Mappers
+{
+  internal static class GitPushUpdateMapper
+  {
+    public static GitPushUpdate FromNativePtr(IntPtr nativeGitPushUpdatePtr)
+    {
+      if (nativeGitPushUpdatePtr == IntPtr.Zero)
+        throw new ArgumentNullException(nameof(nativeGitPushUpdatePtr));
+      using var nativeGitPushUpdate = libgit2.GitPushUpdate.__CreateInstance(nativeGitPushUpdatePtr);
+      return FromNative(nativeGitPushUpdate);
+    }
+
+    public unsafe static GitPushUpdate FromNative(libgit2.GitPushUpdate nativeGitPushUpdate)
+    {
+      return new(
+        SourceReferenceName: StringUtil.ToString(nativeGitPushUpdate.SrcRefname),
+        DestinationReferenceName: StringUtil.ToString(nativeGitPushUpdate.DstRefname),
+        SourceTarget: GitOidMapper.FromNative(nativeGitPushUpdate.Src),
+        DestinationTarget: GitOidMapper.FromNative(nativeGitPushUpdate.Dst)
+      );
+    }
+  }
+}
