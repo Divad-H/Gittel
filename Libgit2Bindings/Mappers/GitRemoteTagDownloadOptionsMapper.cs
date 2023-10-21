2b509e1efac6243abbc708076980606be76b5957
David Hübscher <huebschersdavid@gmail.com>
2023-10-21 02:31:09 +0200

    Add GitTagDownloadOptionsMapper.

---
+++ Libgit2Bindings/Mappers/GitRemoteTagDownloadOptionsMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Libgit2Bindings.Mappers;
+
+internal static class GitRemoteTagDownloadOptionsMapper
+{
+  public static libgit2.GitRemoteAutotagOptionT ToNative(this GitRemoteTagDownloadOptions managedOptions)
+  {
+    return managedOptions switch
+    {
+      GitRemoteTagDownloadOptions.Unspecified => libgit2.GitRemoteAutotagOptionT.GIT_REMOTE_DOWNLOAD_TAGS_UNSPECIFIED,
+      GitRemoteTagDownloadOptions.Auto => libgit2.GitRemoteAutotagOptionT.GIT_REMOTE_DOWNLOAD_TAGS_AUTO,
+      GitRemoteTagDownloadOptions.None => libgit2.GitRemoteAutotagOptionT.GIT_REMOTE_DOWNLOAD_TAGS_NONE,
+      GitRemoteTagDownloadOptions.All => libgit2.GitRemoteAutotagOptionT.GIT_REMOTE_DOWNLOAD_TAGS_ALL,
+      _ => throw new ArgumentOutOfRangeException(nameof(managedOptions), managedOptions, null)
+    };
+  }
+}
