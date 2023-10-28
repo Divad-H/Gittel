3089cc4f29c9fb67bf60a66f4a8cce0507db6e41
David Hübscher <huebschersdavid@gmail.com>
2023-10-29 01:11:07 +0200

    Add the class GitDiff.

---
+++ Libgit2Bindings/GitDiff.cs
@@ -0,0 +1,39 @@
+﻿namespace Libgit2Bindings;
+
+internal class GitDiff : IGitDiff
+{
+  public libgit2.GitDiff NativeGitDiff { get; }
+  private bool _ownsNativeInstance;
+
+  public GitDiff(libgit2.GitDiff nativeGitDiff, bool ownsNativeInstance = true)
+  {
+    NativeGitDiff = nativeGitDiff;
+    _ownsNativeInstance = ownsNativeInstance;
+  }
+
+  #region IDisposable Support
+  private bool _disposedValue;
+  private void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (_ownsNativeInstance)
+      {
+        libgit2.diff.GitDiffFree(NativeGitDiff);
+      }
+      _disposedValue = true;
+    }
+  }
+
+  ~GitDiff()
+  {
+    Dispose(disposing: false);
+  }
+
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+  #endregion
+}
