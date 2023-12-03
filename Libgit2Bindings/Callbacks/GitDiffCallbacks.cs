28cbaf33049c0eff3781ea6e815b5c4f3589d471
David Hübscher <huebschersdavid@gmail.com>
2023-12-03 01:39:23 +0100

    Add GitDiffCallbacks.

---
+++ Libgit2Bindings/Callbacks/GitDiffCallbacks.cs
@@ -0,0 +1,111 @@
+﻿using Libgit2Bindings.Mappers;
+using Libgit2Bindings.Util;
+using System.Runtime.InteropServices;
+
+namespace Libgit2Bindings.Callbacks;
+
+internal class GitDiffCallbacks : IDisposable
+{
+  private readonly Func<GitDiffDelta, float, GitOperationContinuation>? _fileCallback;
+  private readonly Func<GitDiffDelta, GitDiffBinary, GitOperationContinuation>? _binaryCallback;
+  private readonly Func<GitDiffDelta, GitDiffHunk, GitOperationContinuation>? _hunkCallback;
+  private readonly Func<GitDiffDelta, GitDiffHunk, GitDiffLine, GitOperationContinuation>? _lineCallback;
+
+  private readonly GCHandle _gcHandle;
+
+  public GitDiffCallbacks(
+    Func<GitDiffDelta, float, GitOperationContinuation>? fileCallback,
+    Func<GitDiffDelta, GitDiffBinary, GitOperationContinuation>? binaryCallback,
+    Func<GitDiffDelta, GitDiffHunk, GitOperationContinuation>? hunkCallback,
+    Func<GitDiffDelta, GitDiffHunk, GitDiffLine, GitOperationContinuation>? lineCallback)
+  {
+    _fileCallback = fileCallback;
+    _binaryCallback = binaryCallback;
+    _hunkCallback = hunkCallback;
+    _lineCallback = lineCallback;
+
+    _gcHandle = GCHandle.Alloc(this);
+  }
+
+  public IntPtr Payload => GCHandle.ToIntPtr(_gcHandle);
+
+  public static int GitDiffFileCb(IntPtr delta, float progress, IntPtr payload)
+  {
+    Func<GitOperationContinuation> func = () =>
+    {
+      GCHandle gcHandle = GCHandle.FromIntPtr(payload);
+      var callbacks = (GitDiffCallbacks)gcHandle.Target!;
+
+      using var gitDiffDelta = libgit2.GitDiffDelta.__CreateInstance(delta);
+      var deltaToAdd = GitDiffDeltaMapper.FromNative(gitDiffDelta);
+
+      return callbacks._fileCallback?.Invoke(deltaToAdd, progress) ?? GitOperationContinuation.Continue;
+    };
+
+    return func.ExecuteInTryCatch(nameof(GitDiffFileCb));
+  }
+
+  public static int GitDiffBinaryCb(IntPtr delta, IntPtr binary, IntPtr payload)
+  {
+    Func<GitOperationContinuation> func = () =>
+    {
+      GCHandle gcHandle = GCHandle.FromIntPtr(payload);
+      var callbacks = (GitDiffCallbacks)gcHandle.Target!;
+
+      using var gitDiffDelta = libgit2.GitDiffDelta.__CreateInstance(delta);
+      var deltaToAdd = GitDiffDeltaMapper.FromNative(gitDiffDelta);
+
+      using var gitDiffBinary = libgit2.GitDiffBinary.__CreateInstance(binary);
+      var binaryToAdd = GitDiffBinaryMapper.FromNative(gitDiffBinary);
+
+      return callbacks._binaryCallback?.Invoke(deltaToAdd, binaryToAdd) ?? GitOperationContinuation.Continue;
+    };
+
+    return func.ExecuteInTryCatch(nameof(GitDiffBinaryCb));
+  }
+
+  public static int GitDiffHunkCb(IntPtr delta, IntPtr hunk, IntPtr payload)
+  {
+    Func<GitOperationContinuation> func = () =>
+    {
+      GCHandle gcHandle = GCHandle.FromIntPtr(payload);
+      var callbacks = (GitDiffCallbacks)gcHandle.Target!;
+
+      using var gitDiffDelta = libgit2.GitDiffDelta.__CreateInstance(delta);
+      var deltaToAdd = GitDiffDeltaMapper.FromNative(gitDiffDelta);
+
+      using var gitDiffHunk = libgit2.GitDiffHunk.__CreateInstance(hunk);
+      var hunkToAdd = GitDiffHunkMapper.FromNative(gitDiffHunk);
+
+      return callbacks._hunkCallback?.Invoke(deltaToAdd, hunkToAdd) ?? GitOperationContinuation.Continue;
+    };
+
+    return func.ExecuteInTryCatch(nameof(GitDiffHunkCb));
+  }
+
+  public static int GitDiffLineCb(IntPtr delta, IntPtr hunk, IntPtr line, IntPtr payload)
+  {
+    Func<GitOperationContinuation> func = () =>
+    {
+      GCHandle gcHandle = GCHandle.FromIntPtr(payload);
+      var callbacks = (GitDiffCallbacks)gcHandle.Target!;
+
+      using var gitDiffDelta = libgit2.GitDiffDelta.__CreateInstance(delta);
+      var deltaToAdd = GitDiffDeltaMapper.FromNative(gitDiffDelta);
+
+      using var gitDiffHunk = libgit2.GitDiffHunk.__CreateInstance(hunk);
+      var hunkToAdd = GitDiffHunkMapper.FromNative(gitDiffHunk);
+
+      using var gitDiffLine = libgit2.GitDiffLine.__CreateInstance(line);
+      var lineToAdd = GitDiffLineMapper.FromNative(gitDiffLine);
+
+      return callbacks._lineCallback?.Invoke(deltaToAdd, hunkToAdd, lineToAdd) ?? GitOperationContinuation.Continue;
+    };
+
+    return func.ExecuteInTryCatch(nameof(GitDiffLineCb));
+  }
+  public void Dispose()
+  {
+    _gcHandle.Free();
+  }
+}
